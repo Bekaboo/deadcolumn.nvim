3405445d751c6415742d79bd320b501292e5d65c
bekaboo <kankefengjing@gmail.com>
2025-02-09 02:28:41 -0500

    refactor(configs)!: don't enable in command and terminal mode

--- lua/deadcolumn/configs.lua
+++ lua/deadcolumn/configs.lua
@@ -46,7 +46,7 @@ M.opts = {
   scope = 'line', ---@type string|fun(): integer
   ---@type string[]|boolean|fun(mode: string): boolean
   modes = function(mode)
-    return mode:find('^[ictRss\x13]') ~= nil
+    return mode:find('^[iRss\x13]') ~= nil
   end,
   blending = {
     threshold = 0.5,
